e0041129b32f4960fde993f721c9b6a70d82983e
mopanc <jorgemopanc@icloud.com>
2021-08-04 17:05:50 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import './App.css';
+
 import { Component } from 'react'
+
+
+import { loadPosts } from './utils/load-posts'
+import { Posts } from './components/Posts'
+
 
 class App extends Component {
   state = {
-    posts: [],
+    posts: []
   };
 
-  componentDidMount() {
-    this.LoadPosts();
+  async componentDidMount() {
+    await this.loadPosts();
   }
 
-  LoadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
-
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url }
-    });
-
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos });
   }
 
@@ -31,18 +26,7 @@ class App extends Component {
 
     return (
       <section className="container">
-        <div className="posts">
-          {posts.map(post => (
-            <div className="post">
-              <img src={post.cover} alt={post.title} />
-              <div key={post.id} className="post-content">
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-              </div>
-            </div>
-
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
 
     );
