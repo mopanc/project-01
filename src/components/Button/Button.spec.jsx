dac32067370038ed3f97e0f70af848b9620c34ba
mopanc <jorgemopanc@icloud.com>
2021-08-10 13:37:57 -0300

    testing button

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -1,8 +1,41 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Button } from '.';
 
 describe('<Button />', () => {
-  it('should render the button with the text', () => {
-    render(<Button />);
+  it('should render the button with the text "Load More"', () => {
+    render(<Button text="Load More" />);
+
+    expect.assertions(1);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should call function on button click', () => {
+    const fn = jest.fn();
+    render(<Button text="Load More" onClick={fn} />);
+
+
+    const button = screen.getByRole('button', { name: /load more/i });
+
+    fireEvent.click(button);//pode ser usado um ou outro não faz diferença
+    // || userEvent.click(button);
+
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be disabled when disabled is true', () => {
+    render(<Button text="Load More" disabled={true} />);
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeDisabled();
+  });
+
+  it('should be enabled when disabled is false', () => {
+    render(<Button text="Load More" disabled={false} />);
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeEnabled();
   });
 });
+
+// npm test -- --coverage serve para verificar o coverage dos seus testes, ele 
+//verifica se esta faltando algum teste na sua logica e te avisa verifica o index
